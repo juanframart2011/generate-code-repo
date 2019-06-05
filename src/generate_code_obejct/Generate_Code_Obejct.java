2b44607f4c4bb5c7b71924bb6a90549225104bda
juanframart2011 <juanframart2011@gmail.com>
2019-06-05 17:39:01 -0500

    Agregar funciones de lectura y escritura de archivo

--- src/generate_code_obejct/Generate_Code_Obejct.java
+++ src/generate_code_obejct/Generate_Code_Obejct.java
@@ -21,5 +21,55 @@ public class Generate_Code_Obejct {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }   
+        
+        //Un texto cualquiera guardado en una variable
+        String saludo="Hola";
+
+        try
+        {
+            //Crear un objeto File se encarga de crear o abrir acceso a un archivo que se especifica en su constructor
+            File archivo=new File("texto.txt");
+
+            //Crear objeto FileWriter que sera el que nos ayude a escribir sobre archivo
+            FileWriter escribir=new FileWriter(archivo,true);
+
+            //Escribimos en el archivo con el metodo write 
+            escribir.write(saludo);
+
+            //Cerramos la conexion
+            escribir.close();
+        }
+
+        //Si existe un problema al escribir cae aqui
+        catch(Exception e)
+        {
+            System.out.println("Error al escribir");
+        }
+    }
+    
+    public static void escribir(String arg[]){
+        //Creamos un String que va a contener todo el texto del archivo
+        String texto="";
+
+        try
+        {
+            //Creamos un archivo FileReader que obtiene lo que tenga el archivo
+            FileReader lector=new FileReader("texto_escrito.txt");
+
+            //El contenido de lector se guarda en un BufferedReader
+            BufferedReader contenido=new BufferedReader(lector);
+
+            //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+            while((texto=contenido.readLine())!=null)
+            {
+                System.out.println(texto);
+            }
+        }
+
+        //Si se causa un error al leer cae aqui
+        catch(Exception e)
+        {
+            System.out.println("Error al leer");
+        }
+    }
 }
